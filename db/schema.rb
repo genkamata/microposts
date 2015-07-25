220843fc10fac34a73a9366244f7bde88f7c9442
genkamata <kamagen0426@gmail.com>
2015-07-25 09:35:17 +0000

    menter help

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150719005157) do
+ActiveRecord::Schema.define(version: 20150725090825) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,9 @@ ActiveRecord::Schema.define(version: 20150719005157) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "sex"
+    t.string   "country"
+    t.integer  "age"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
