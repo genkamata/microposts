e42fa7a0fdbf7f7fe1694ab07fcbaa32825b2dc3
genkamata <kamagen0426@gmail.com>
2015-08-01 07:44:41 +0000

    20150801

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
   has_many :followed_users, through: :followed_relationships, source: :follower
   
   # paperclip用
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   
   # 他のユーザーをフォローする
--- db/migrate/20150719005157_create_users.rb
+++ db/migrate/20150719005157_create_users.rb
@@ -5,6 +5,8 @@ class CreateUsers < ActiveRecord::Migration
       t.string :email
       t.string :password_digest
       
+      t.attachment :avatar
+
       t.timestamps
 
       t.index :email, unique: true # この行を追加
